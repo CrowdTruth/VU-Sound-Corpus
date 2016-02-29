7ae8dad580b670200e04beb87986f1130fb944f2
Emiel <emielonline@gmail.com>
2016-02-29 15:04:58 +0100

    Change bar chart

--- scripts/9-barchart.py
+++ scripts/9-barchart.py
@@ -4,11 +4,12 @@ import csv
 from lxml import etree
 import matplotlib.pyplot as plt
 from collections import Counter
-import pandas as pd
-
-pd.options.display.mpl_style = 'default'
-plt.rcParams['font.family'] = ['sans-serif']
-
+import seaborn as sns
+# import pandas as pd
+#
+# pd.options.display.mpl_style = 'default'
+plt.rcParams['figure.figsize'] = [8.0, 2.0]
+sns.set_style("whitegrid")
 def searchterms():
     "Generator that yields tuples of search terms (str) and their counts (int)"
     with open('../resources/searchterms/Analytics Search page Search Terms 20150316-20150818.csv') as f:
@@ -42,31 +43,47 @@ raw_tag_counter = Counter(all_xpath_labels('.//crowd-tags/tag/raw'))
 total_raw = float(sum(raw_tag_counter.values()))
 
 # Get all groups.
-just_author = (all_author_tags - all_search_terms) & all_raw_tags
-just_search = (all_search_terms - all_author_tags) & all_raw_tags
+author = all_author_tags & all_raw_tags
+search = all_search_terms & all_raw_tags
 author_and_search = (all_author_tags & all_search_terms) & all_raw_tags
 rest = all_raw_tags - (all_author_tags | all_search_terms)
 
 # Get percentages.
-just_author_num = (number_in_set(raw_tag_counter, just_author) / total_raw) * 100
-just_search_num = (number_in_set(raw_tag_counter, just_search) / total_raw) * 100
+author_num = (number_in_set(raw_tag_counter, author) / total_raw) * 100
+search_num = (number_in_set(raw_tag_counter, search) / total_raw) * 100
 author_and_search_num = (number_in_set(raw_tag_counter, author_and_search) / total_raw) * 100
 rest_num = (number_in_set(raw_tag_counter, rest) / total_raw) * 100
 
 # Put them in a dictionary.
-d = {'Groups': ["Author tags", "Search terms", "Both", "Rest"],
-     'Percentage': [just_author_num, just_search_num, author_and_search_num, rest_num]}
+# d = {'Groups': ["Author tags", "Search terms", "Both", "Rest"],
+#      'Percentage': [author_num, search_num, author_and_search_num, rest_num]}
+#
+# print(list(zip(*d.values())))
 
-df = pd.DataFrame([[just_author_num, just_search_num, author_and_search_num, rest_num]],
-                  columns=["Author tags", "Search terms", "Both", "Rest"])
+# df = pd.DataFrame([[author_num, search_num, author_and_search_num, rest_num]],
+#                   columns=["Author tags", "Search terms", "Both", "Rest"])
+#
+# bar_chart = df.plot(kind='barh', width=0.1, label=None);
 
-bar_chart = df.plot(kind='barh', stacked=True, width=0.1, label=None, figsize=(4,2));
 
-for container in bar_chart.containers:
-    container.get_children()[0].set_y(-0.25)
+sns.set_context({"figure.figsize": (8,2)})
 
-bar_chart.grid(axis='y',b=False)
-bar_chart.set_yticklabels([''])
+sns.barplot([author_num, search_num, rest_num],
+            ["Author tags", "Search terms", "Rest"],
+            color="steelblue")
+
+plt.axvline(55,linestyle='dotted', color="white")
+
+# bla = sns.barplot([author_and_search_num,author_and_search_num,0],
+#                   ["Author tags", "Search terms", "Rest"],
+#                   color='lightsteelblue')
+
+# plt.text(1,0.15, "\% of crowd tags in in both author tags and search terms")
+# for container in bar_chart.containers:
+#     container.get_children()[0].set_y(-0.25)
+#
+# bar_chart.grid(axis='y',b=False)
+# bar_chart.set_yticklabels([''])
 # Create bar chart.
 #ax = sns.barplot(x="Groups", y="Percentage", data=d, color='dodgerblue')
 plt.savefig('../steps/4-results/figures/barchart.pdf')
